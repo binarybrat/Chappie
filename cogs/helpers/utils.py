cfe766815d3a82a80d739cd0d4c99ff5e64cdbe9
philzeey <trevorcoreyphillips@gmail.com>
2017-12-18 12:28:41 -0700

    added roles for the point system

--- cogs/helpers/utils.py
+++ cogs/helpers/utils.py
@@ -5,6 +5,9 @@ from . import point_system
 thingsToSay = ['What?', 'That\'s me', 'Hello!',
                'That\'s my name, don\'t wear it out']
 
+rank = ['Level 1: Server Newbie', 'Level 2: Server Apprentice', 'Level 3: Server Helper', 'Level 4: Server Contributor', 'Level 5: Server Legend']
+ranks = {'Level: 1': 100, 'Level: 2': 250, 'Level: 3': 500, 'Level: 4': 1000, 'Level: 5': 2500}
+
 
 async def findChannelObject(guild, channel_name):
     for channel in guild.channels:
@@ -19,6 +22,7 @@ async def findRoleObject(guild, role_name):
 
 
 async def point_system_check(message):
+    guild = message.guild
     if not message.author.bot:
         pt = point_system
         if not pt.check_if_user_in_table(message):
@@ -26,5 +30,52 @@ async def point_system_check(message):
         que = pt.query_points(message)
         points = que[0].num_points
         points += 1
-        print(points)
         pt.update_table(message, points)
+
+        if 100 < points <= 249:
+
+            role1 = await findRoleObject(guild, rank[0])
+            if not role1 in message.author.roles:
+                msg = 'Congrats {1.author.mention}, you have achieved {0.name}! The more you contribute to any server chat the more XP you get :). You will have to gain 150 more XP to gain another level! :robot:'
+                await message.author.add_roles(role1)
+                await message.channel.send(msg.format(role1, message))
+
+        elif 250 <= points <= 499:
+
+            role1 = await findRoleObject(guild, rank[1])
+            role2 = await findRoleObject(guild, rank[1 - 1])
+            if not role1 in message.author.roles:
+                msg = 'Congrats {1.author.mention}, you have achieved {0.name}! The more you contribute to any server chat the more XP you get :). You will have to gain 250 more XP to gain another level! :robot:'
+                await message.author.remove_roles(role2)
+                await message.author.add_roles(role1)
+                await message.channel.send(msg.format(role1, message))
+
+        elif 500 <= points <= 999:
+
+            role1 = await findRoleObject(guild, rank[2])
+            role2 = await findRoleObject(guild, rank[2 - 1])
+            if not role1 in message.author.roles:
+                msg = 'Congrats {1.author.mention}, you have achieved {0.name}! The more you contribute to any server chat the more XP you get :). You will have to gain 750 more XP to gain another level! :robot:'
+                await message.author.remove_roles(role2)
+                await message.author.add_roles(role1)
+                await message.channel.send(msg.format(role1, message))
+
+        elif 1000 <= points <= 2499:
+
+            role1 = await findRoleObject(guild, rank[3])
+            role2 = await findRoleObject(guild, rank[3 - 1])
+            if not role1 in message.author.roles:
+                msg = 'Congrats {1.author.mention}, you have achieved {0.name}! The more you contribute to any server chat the more XP you get :). You will have to gain 1500 more XP to gain another level! :robot:'
+                await message.author.remove_roles(role2)
+                await message.author.add_roles(role1)
+                await message.channel.send(msg.format(role1, message))
+
+        elif 2500 <= points:
+
+            role1 = await findRoleObject(guild, rank[4])
+            role2 = await findRoleObject(guild, rank[4 - 1])
+            if not role1 in message.author.roles:
+                msg = 'Congrats {1.author.mention}, you have achieved {0.name}! The more you contribute to any server chat the more XP you get :). You have reached maximum level! :robot:'
+                await message.author.remove_roles(role2)
+                await message.author.add_roles(role1)
+                await message.channel.send(msg.format(role1, message))
