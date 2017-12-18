19190e55df3f482a4aae73240fdd1fa3516018fc
philzeey <trevorcoreyphillips@gmail.com>
2017-12-17 23:14:48 -0800

    made point system check its own def

--- cogs/helpers/utils.py
+++ cogs/helpers/utils.py
@@ -1,5 +1,6 @@
 import sqlite3
 import random
+from . import point_system
 
 thingsToSay = ['What?', 'That\'s me', 'Hello!',
                'That\'s my name, don\'t wear it out']
@@ -15,3 +16,15 @@ async def findRoleObject(guild, role_name):
     for role in guild.roles:
         if role.name == role_name:
             return role
+
+
+async def point_system_check(message):
+    if not message.author.bot:
+        pt = point_system
+        if not pt.check_if_user_in_table(message):
+            pt.insert_user_in_table(message)
+        que = pt.query_points(message)
+        points = que[0].num_points
+        points += 1
+        print(points)
+        pt.update_table(message, points)
