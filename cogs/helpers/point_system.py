df1dc8beabffd1638d2caca60040406b626f78ce
philzeey <trevorcoreyphillips@gmail.com>
2017-12-17 23:14:36 -0800

    took create table out of def

--- cogs/helpers/point_system.py
+++ cogs/helpers/point_system.py
@@ -20,31 +20,39 @@ class Points(Base):
     server_id = Column(Integer, nullable=False)
 
 
-def create_table():
-    Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
+
 
 def get_top_5(guild):
     return session.query(Points).filter(Points.server_id == int(guild.id)).order_by(Points.num_points.desc()).limit(5)
 
+
 def insert_user_in_table(msg_obj):
-    new_submission = Points(username=str(msg_obj.author), num_points=0, server_name=str(msg_obj.guild.name), server_id=int(msg_obj.guild.id))
+    new_submission = Points(username=str(msg_obj.author), num_points=0, server_name=str(msg_obj.guild.name),
+                            server_id=int(msg_obj.guild.id))
     session.add(new_submission)
     session.commit()
     session.close()
 
+
 def query_points(msg_obj):
-    return session.query(Points).filter(Points.username == str(msg_obj.author), Points.server_id == int(msg_obj.guild.id)).all()
+    return session.query(Points).filter(Points.username == str(msg_obj.author),
+                                        Points.server_id == int(msg_obj.guild.id)).all()
+
 
 def check_if_user_in_table(msg_obj):
     session = DBSession()
-    if session.query(Points).filter(Points.username == str(msg_obj.author), Points.server_id == int(msg_obj.guild.id)).all():
+    if session.query(Points).filter(Points.username == str(msg_obj.author),
+                                    Points.server_id == int(msg_obj.guild.id)).all():
         return True
     else:
         return False
 
+
 def update_table(msg_obj, points):
     update_submission = update(Points). \
-        where(and_(Points.username == str(msg_obj.author), Points.server_id == int(msg_obj.guild.id))).values(num_points=points)
+        where(and_(Points.username == str(msg_obj.author), Points.server_id == int(msg_obj.guild.id))).values(
+        num_points=points)
     session.execute(update_submission)
     session.commit()
     session.close()
